fc7c57cf41dff7dc1148cb7b1aff6a63e836def5
ipanfilenko <Igor.Panfilenko@wolterskluwer.com>
2021-06-20 18:52:34 +0300

    feat: support env file

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,12 +1,14 @@
 import { ConnectionOptions } from "typeorm";
 
+const { DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS} = process.env;
+
 export const typeormConfig: ConnectionOptions = {
    "type": "postgres",
-   "host": "localhost",
-   "port": 5433,
-   "username": "postgres",
-   "password": "1234",
-   "database": "trello",
+   "host": DB_HOST,
+   "port": +DB_PORT!,
+   "username": DB_USER,
+   "password": DB_PASS,
+   "database": DB_NAME,
    "synchronize": true,
    "logging": false,
    "entities": [
